9e5cfd3e4fe570d4a884a28b82fe034777d559a8
Dan0dy <49503853+Dan0dy@users.noreply.github.com>
2019-10-17 01:49:43 +0200

    Update and rename leetcode.py to hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -8,4 +8,12 @@ def h(n, A, B, C):
         print('%s -> %s' %(A, C))  #把第n个从A移动到c
         h(n-1,B, A, C)   #再把前n-1个从B通过A移到C
 #h(n) = 2h(n-1) +1
+
+def h1(n):
+    if n == 1:
+        return 1
+    elif n > 1:
+        return 2*h1(n-1) + 1
 h(3, 'A', 'B','C')
+print(h1(3))
+
